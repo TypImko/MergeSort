bd068bc4d9eaa5b6c44fdb1e81d4d7be67676a8d
Mathew Shimko <nokminer@gmail.com>
2025-02-09 16:32:16 -0500

    SHLERM file update

--- bash.sh
+++ bash.sh
@@ -1,9 +1,4 @@
 !/bin/sh
-#SBATCH --MergeSortJob
-#SBATCH --partition=Centaurus
-#SBATCH --time=00:10:00
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-
 #This script will be read be SLURM for arguments, executed by the requested nodes.
-work
+make ms
+./ms > Merge_Answer
