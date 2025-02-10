38f0a58f2e70c3d1bcab8e3bedac290cad10db75
Mathew Shimko <nokminer@gmail.com>
2025-02-10 14:57:38 -0500

    fixes

--- bash.sh
+++ bash.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 #This script will be read be SLURM for arguments, executed by the requested nodes.
-work
-
+g++ ms.cpp -o Merge
+./Merge 10
+./Merge 100
+./Merge 1000
+./Merge 10000
+./Merge 100000
+./Merge 1000000
+./Merge 10000000
+./Merge 100000000
+./Merge 1000000000
